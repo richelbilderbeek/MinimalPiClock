01bc04930f3e36bfaa10f3af91d02e2acc236aff
richelbilderbeek <richel@richelbilderbeek.nl>
2019-11-03 18:03:45 +0100

    Simplified

--- src/src.ino
+++ src/src.ino
@@ -6,6 +6,7 @@
 2015-07-05: v.1.0: Initial version
 2015-08-21: v.1.1: Added beep if A0 is connected to GND at startup
 2019-07-19: v.1.2: Always beep at startup
+2019-11-03: v.1.3: Plug in at Pi o'clock instead of at midnight
 
 Piezo:
   
@@ -28,22 +29,6 @@ Piezo:
 
 const int pin_piezo = 8; //The pin connected to the piezo
 
-//The pin connected to the error LED
-const int pin_error = 13; 
-
-void OnError(const String& error_message)
-{
-  Serial.print("ERROR: ");  
-  Serial.println(error_message);  
-  while (1)
-  {
-    //Blink LED
-    digitalWrite(pin_error,!digitalRead(pin_error));
-    //Write to serial
-    delay(1000);
-  }
-}
-
 void TestTime()
 {
   //  setTime(hours, minutes, seconds, days, months, years);
@@ -54,25 +39,22 @@ void TestTime()
   const int months = 0;
   const int years = 0;
   setTime(hours, minutes, seconds, days, months, years);
-  if (hour() != hours) { OnError("TestTime #1"); }
-  if (minute() != minutes) { OnError("TestTime #1"); }
-  if (second() != seconds) { OnError("TestTime #1"); }
-  setTime(0,0,0,0,0,0);
+  setTime(15,14,0,0,0,0);
+}
+
+void beep()
+{
+  const int frequency_hz = 3142;
+  const int duration_msec = 3142;
+  tone(pin_piezo, frequency_hz, duration_msec);
 }
 
 void setup() 
 {
   pinMode(pin_piezo, OUTPUT);
-  pinMode(pin_error, OUTPUT);
-  Serial.begin(9600); //Cannot be used: chip is used stand-alone
-  #ifndef NDEBUG
-  Serial.println("PiClock v. 1.2 (debug version)");
-  #else //NDEBUG
-  Serial.println("PiClock v. 1.2 (release version)");
-  #endif //NDEBUG
   TestTime();
-  
-  tone(pin_piezo,3142,314);
+  //No need to do a beep here: the sketch starts at pi o'clock and will detect this
+  //beep(); 
 }
 
 void loop() 
@@ -102,9 +84,7 @@ void loop()
       if (!is_pi_oclock)
       {
         is_pi_oclock = true;
-        const int frequency_hz = 3142;
-        const int duration_msec = 3142;
-        tone(pin_piezo,frequency_hz,duration_msec);
+        beep();
       }
     }
     else 
